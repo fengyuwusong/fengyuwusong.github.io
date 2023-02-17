4518b9d8a74f95e124d92dc2d248a6e72b887971
fengyuwusong <fyws@fengyuwusong.cn>
2023-02-17 16:53:03 +0800

    feat: 更改博客框架为vuepress-vdoing

---
+++ docs/.vuepress/components/LastReading.vue
@@ -0,0 +1,34 @@
+<template>
+  <LastReadingPopup v-slot="{ show, goto, message, buttonText }">
+    <div v-if="show" class="my-sw-update-popup">
+      {{ message }}<br>
+      <button @click="goto">{{ buttonText }}</button>
+    </div>
+  </LastReadingPopup>
+</template>
+
+<script>
+import LastReadingPopup from 'vuepress-plugin-last-reading/src/LastReadingPopup.vue'
+
+export default {
+  components: { LastReadingPopup }
+}
+</script>
+
+<style>
+.my-sw-update-popup {
+  text-align: right;
+  position: fixed;
+  bottom: 100px;
+  right: 20px;
+  background-color: #fff;
+  font-size: 20px;
+  padding: 10px;
+  border: 5px solid #3f3e3e;
+  border-radius: 10px;
+}
+
+.my-sw-update-popup button {
+  border: 1px solid #fefefe;
+}
+</style>
---
+++ docs/.vuepress/config/baiduCode.ts
@@ -0,0 +1,1 @@
+export default '278b32b498fb6b27b971eeb27c868804'
